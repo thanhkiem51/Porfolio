747f82c2c9722602ffde1a95c51e2e43e02e5267
thanhkiem51 <nguyenbrother9x@gmail.com>
2018-06-24 12:51:56 -0700

    add practice

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-// git add --all
-// git commit -m "{summary}"
-// git push
-var application = angular.module('info', []);
-application.controller('mycontroller', function($scope) {
 
-});
+var contentApp = angular.module('contentApp', []);
+
+
+
