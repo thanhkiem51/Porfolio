e5e6b7aec089491ba6d56e360aec138c1b77df7e
thanhkiem51 <nguyenbrother9x@gmail.com>
2018-06-26 13:04:05 -0700

    finished navigation bar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
+$(document).ready(function() {
+	$('.fa-bars').on('click',function () {
+		var x = document.getElementById("nav-bar");
+		if (x.className === 'nav-bar')
+			x.className += ' responsive';
+		else
+			x.className = 'nav-bar';
+	})
 
-var contentApp = angular.module('contentApp', []);
+});
+
+
+// var contentApp = angular.module('contentApp', []);
 
 
 
