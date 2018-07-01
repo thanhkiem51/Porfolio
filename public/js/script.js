7b79ab9048318a6a3ecf697c4d6a74c5b58e4274
thanhkiem51 <nguyenbrother9x@gmail.com>
2018-07-01 14:55:22 -0700

    make it better

---
+++ public/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$('.fa-bars').on('click',function () {
+		var x = document.getElementById("nav-bar");
+		if (x.className === 'nav-bar')
+			x.className += ' responsive';
+		else
+			x.className = 'nav-bar';
+	})
+	// $('#content').load("http://cors.io/?https://vnexpress.net/");
+});
+
+// var contentApp = angular.module('contentApp', []);
+// contentApp.config(function($routeProvider) {
+// 	routeProvider
+// 	.when("/", {templateUrl: "home.html"})
+// });
+// contentApp.controller('mainController', function($scope) {
+
+// });
+
+
---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 3000
+
+app.get('/', (request, response) => {
+  response.send('Hello from Express!');
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err);
+  }
+
+  console.log(`server is listening on ${port}`);
+})
+// 'use strict';
+// var express = require('express'),
+//   bodyParser = require('body-parser'),
+//   path = require('path')
+//   // Datastore = require('nedb');
+
+// var app = express();
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// var port = process.env.PORT || 5000;
+
+// app.use('/', express.static('public'));
+
+// app.listen(port);
+// console.log('Magic happens on port ' + port);
+// alert("hey");
