e7a9b84c102fc4d14ea594ce0c08e8661a7857f5
thanhkiem51 <nguyenbrother9x@gmail.com>
2018-07-11 17:23:59 -0700

    make it better

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,7 @@
 $(document).ready(function() {
 	// first load on delay
-	// $('#line1').hide().delay(3000).show(2200);
-	// $('#line2').hide().delay(5000).show(2200);
-	// $('#welcome-button').hide().delay(10000).show(2200);
+	
 
-	// $('#welcome-button').hide().delay(8000).show(2200);
 
 	//responsive manipulator
 	$('.fa-bars').on('click',function () {
@@ -18,13 +15,13 @@ $(document).ready(function() {
 
 
 	// jquery for front page
-	// $('#greet').click(function() {
-	// 	var audio = document.getElementById('audio');
-	// 	if (audio.paused) {
- //            audio.play();
- //        }else{
- //            audio.pause();
- //            audio.currentTime = 0
- //        }
-	// })
+	$('#greet').click(function() {
+		var audio = document.getElementById('audio');
+		if (audio.paused) {
+            audio.play();
+        }else{
+            audio.pause();
+            audio.currentTime = 0
+        }
+	})
 });
